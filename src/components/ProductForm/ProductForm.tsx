e0d730de2561469cbfa834e5c4fce01ac69fc11f
Sujata Singh <sujatasingh@Sujatas-Air.lan>
2025-04-27 22:25:03 +0530

    seperate product types

--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -1,28 +1,6 @@
 import Dropdown from "../DropDown/DropDown";
 import TextInput from "../TextInput/TextInput";
-
-type ProductFormProps = {
-  formData: {
-    title: string;
-    category: string;
-    tags: string;
-    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
-  };
-  errors: {
-    title?: string;
-    category?: string;
-    tags?: string;
-  };
-  handleChange: (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => void;
-  handleSubmit: (e: React.FormEvent) => void;
-  isSubmitting: boolean;
-  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
-  handleBlur: (
-    e: React.FocusEvent<HTMLInputElement | HTMLSelectElement>
-  ) => void;
-};
+import { ProductFormProps } from "./types";
 
 const ProductForm: React.FC<ProductFormProps> = ({
   formData,
--- src/components/ProductForm/types.ts
+++ src/components/ProductForm/types.ts
@@ -4,3 +4,26 @@ export type ProductFormData = {
     tags: string;
     images: File[];
 };
+
+export type ProductFormProps = {
+    formData: {
+        title: string;
+        category: string;
+        tags: string;
+        onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+    };
+    errors: {
+        title?: string;
+        category?: string;
+        tags?: string;
+    };
+    handleChange: (
+        e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => void;
+    handleSubmit: (e: React.FormEvent) => void;
+    isSubmitting: boolean;
+    onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+    handleBlur: (
+        e: React.FocusEvent<HTMLInputElement | HTMLSelectElement>
+    ) => void;
+};
